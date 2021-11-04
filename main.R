e8693cfabe0bd8a92f0f86462fc8d91df6739aa1
wptmdoorn <wptmdoorn@gmail.com>
2021-11-04 15:01:35 +0000

    Update core functionality

--- main.R
+++ main.R
@@ -1,11 +1,39 @@
 library(tercen)
 library(dplyr, warn.conflicts = FALSE)
+library(flowCore)
 
-ctx = tercenCtx()
 
-ctx %>%
-  select(.y, .ci, .ri) %>% 
+do.scale = function(df, ...) {
+  biexp  <- biexponentialTransform("bi_exponential")
+  scale.m = try(transform(new("flowFrame", exprs = as.matrix(df)), `.y` =
+                            biexp(`.y`))@exprs[, 3],
+                silent = TRUE)
+  
+  if(inherits(scale.m, 'try-error')) {
+    return (data.frame(.ri = integer(),
+                       .ci = integer(),
+                       scale=double()))
+  }
+  
+  result = as_tibble(scale.m) %>%
+    rename_all(
+      .funs = function(x)
+        'transform'
+    ) %>%
+    mutate(.ri = df$.ri, .ci = df$.ci)
+  
+  return (result)
+}
+
+(ctx = tercenCtx()) %>%
+  select(.ci, .ri, .y) %>%
   group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+  summarise(.y = mean(.y)) %>%
+  group_by(.ri) %>%
+  do(do.scale(
+    .,
+    scale = as.logical(ctx$op.value('scale')),
+    center = as.logical(ctx$op.value('center'))
+  )) %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,17 +1,46 @@
 library(tercen)
 library(dplyr, warn.conflicts = FALSE)
+library(flowCore)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+# http://localhost:5402/admin/w/93fd4674e154604dd39e1863840078ca/ds/b1c658a9-59c0-4416-b8d9-6f735d32333f
+
+options("tercen.workflowId" = "93fd4674e154604dd39e1863840078ca")
+options("tercen.stepId"     = "b1c658a9-59c0-4416-b8d9-6f735d32333f")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-ctx = tercenCtx()
+do.scale = function(df, ...) {
+  biexp  <- biexponentialTransform("bi_exponential")
+  scale.m = try(transform(new("flowFrame", exprs = as.matrix(df)), `.y` =
+                            biexp(`.y`))@exprs[, 3],
+                silent = TRUE)
+  
+  if(inherits(scale.m, 'try-error')) {
+    return (data.frame(.ri = integer(),
+                       .ci = integer(),
+                       scale=double()))
+  }
+  
+  result = as_tibble(scale.m) %>%
+    rename_all(
+      .funs = function(x)
+        'transform'
+    ) %>%
+    mutate(.ri = df$.ri, .ci = df$.ci)
+  
+  return (result)
+}
 
-ctx %>%
-  select(.y, .ci, .ri) %>%
+(ctx = tercenCtx()) %>%
+  select(.ci, .ri, .y) %>%
   group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+  summarise(.y = mean(.y)) %>%
+  group_by(.ri) %>%
+  do(do.scale(
+    .,
+    scale = as.logical(ctx$op.value('scale')),
+    center = as.logical(ctx$op.value('center'))
+  )) %>%
   ctx$addNamespace() %>%
   ctx$save()
