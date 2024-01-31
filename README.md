198e0e9dc0bd2eaba2f45e00592b2a08dad1c800
agouy <alexandre.gouy@tercen.com>
2024-01-31 10:05:42 +0000

    Added biexponential settings.

--- README.md
+++ README.md
@@ -21,7 +21,9 @@ Output relations|.
 ##### Details
 
 The operator is the `biexponential_transformation` function in from the flowCore
-package available in R.
+package available in R. This is an over-parameterized inverse of the hyperbolic sine. 
+
+The function to be inverted takes the form biexp(x) = a*exp(b*(x-w))-c*exp(-d*(x-w))+f with default parameters selected to correspond to the hyperbolic sine.
 
 ##### References
 
--- main.R
+++ main.R
@@ -11,10 +11,17 @@ ctx = tercenCtx()
 df <- ctx$select(list(".ci", ".ri", ".y")) 
 ff <- new("flowFrame", exprs = data.matrix(df))
 
+par_a <- ctx$op.value("a", as.numeric, 0.5)
+par_b <- ctx$op.value("b", as.numeric, 1)
+par_c <- ctx$op.value("c", as.numeric, 0.5)
+par_d <- ctx$op.value("d", as.numeric, 1)
+par_f <- ctx$op.value("f", as.numeric, 0)
+par_w <- ctx$op.value("w", as.numeric, 0)
+
 scale.m = try(
   transform(
     ff, `value` =
-    biexponentialTransform("bi_exponential")(`.y`)
+    biexponentialTransform("bi_exponential", a = par_a, b = par_b, c = par_c, d = par_d, f = par_f, w = par_w)(`.y`)
   )@exprs, silent = FALSE)  %>%
   as_tibble()  %>%
   select(.ci, .ri, value) %>%
